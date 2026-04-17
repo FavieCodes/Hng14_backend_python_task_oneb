ebe2bb4e4ce80a5df38a994ea1d56a8527856559
FavieCodes <imo.e.udoh@gmail.com>
2026-04-17 12:26:34 +0100

    Update database settings.py

--- profilApi_project/settings.py
+++ profilApi_project/settings.py
@@ -77,13 +77,18 @@ WSGI_APPLICATION = 'profilApi_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/tmp/db.sqlite3',  # Railway's writable temp directory
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
